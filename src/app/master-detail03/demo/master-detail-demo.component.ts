d3eb27846f58507d65dae1c5454031898c3b6928
princend <a31136@mail.cmuh.org.tw>
2017-06-09 10:20:50 +0800

    未鬆散耦合

--- src/app/master-detail03/demo/master-detail-demo.component.ts
+++ src/app/master-detail03/demo/master-detail-demo.component.ts
@@ -1,132 +1,91 @@
 import { AfterViewInit, Component, OnChanges, OnInit, ViewChild } from '@angular/core';
-import {Dt,Mt,MasterDetail} from '../models';
+import { Dt, Mt, MasterDetail } from '../models';
 import { MasterDetailComponent } from "../master-detail.component";
 @Component({
-     selector: 'cmuh-master-detail-demo',
-    templateUrl: 'master-detail-demo.component.html'
+  selector: 'cmuh-master-detail-demo',
+  templateUrl: 'master-detail-demo.component.html'
 })
 
-export class MasterDetailDemoComponent implements OnInit,OnChanges {
+export class MasterDetailDemoComponent implements OnInit, OnChanges {
+
+  @ViewChild('md') masterDetail: MasterDetailComponent;
+  constructor() { }
+
+  ngOnInit() { }
+
+  private contentDisplay: boolean = true;
 
 
-    @ViewChild('md') masterDetail: MasterDetailComponent;
-    constructor() { }
-
-
-        ngOnInit() { }
-
-    private contentDisplay:boolean=true;
-
-
-//移植到demo
-
- inputData:MasterDetail[] =[
-{     master: 1 ,detail:[{ c1: 1, c2: 7, c3: 3 },
-  { c1: 2, c2: 5, c3: 6 },
-  { c1: 3, c2: 8,c3: 9 }] },
-  {master:2,detail:[{ c1: 1, c2: 22, c3: 33 },
-  { c1: 21, c2: 55, c3: 66 },
-  { c1: 34, c2: 88, c3: 99 }]}
-
-] 
-
-  displayDialog: boolean;
-  dtdisplayDialog: boolean;
-  warningDisplayDialog: boolean;
-  dtchdisplayDialog: boolean;
-  addmt: Mt = new Mt();
-  //adddt:Dt=new Dt();
-  adddt;
-  private mtIndexValue;
-private testdtColumns: Object[] =
+  private testdtColumns: Object[] =
   [{ "field": "c1", "header": "c1" },
   { "field": "c2", "header": "c2" },
   { "field": "c3", "header": "c3" }];
   ;
+
   private testmtDatas: Mt[] = [
     { "value": 1 },
     { "value": 2 }
   ];
 
-private testdtDatas: Object[] = [];
+  private inputData: MasterDetail[] = [
+    {
+      master: 1, detail: [{ c1: 1, c2: 7, c3: 3 },
+      { c1: 2, c2: 5, c3: 6 },
+      { c1: 3, c2: 8, c3: 9 }]
+    },
+    {
+      master: 2, detail: [{ c1: 1, c2: 22, c3: 33 },
+      { c1: 21, c2: 55, c3: 66 },
+      { c1: 34, c2: 88, c3: 99 }]
+    }
+  ]
+  private displayDialog: boolean;
+  private dtdisplayDialog: boolean;
+  private warningDisplayDialog: boolean;
+  private dtchdisplayDialog: boolean;
+  private addmt: Mt;
+  private adddt: Dt;
 
- private  testdtDatas1: Dt[] =
-  [{ "c1": 1, "c2": 7, "c3": 3 },
-  { "c1": 2, "c2": 5, "c3": 6 },
-  { "c1": 3, "c2": 8, "c3": 9 }];
-
- private  testdtDatas2: Dt[] =
-  [{ "c1": 1, "c2": 22, "c3": 33 },
-  { "c1": 21, "c2": 55, "c3": 66 },
-  { "c1": 34, "c2": 88, "c3": 99 }]
 
   testmtshowDialogToAdd() {
     this.addmt = new Mt();
     this.displayDialog = true;
   }
 
-  testdtshowDialogToAdd(){
+  testdtshowDialogToAdd() {
     this.adddt = new Dt();
     this.dtdisplayDialog = true;
-  
+
   }
 
-mtsave(){
+  mtsave() {
     this.masterDetail.mtsave(this.addmt.value);
-    this.displayDialog=false;
-}
-
-close(){
- this.displayDialog=false;
-  this.dtdisplayDialog=false;
-  this.warningDisplayDialog=false;
-  this.dtchdisplayDialog=false;
-}
-
-
-
-dtsave(){
-     this.masterDetail.dtsave(this.adddt);
-    this.dtdisplayDialog=false;
-}
-
-
-//mt單選一筆交換明細
-/*  public MtRowSelect(event) {
-    this.masterDetail.dtchange();
+    this.displayDialog = false;
   }
-*/
 
-DtRowSelect(event){
-    this.dtchdisplayDialog=true;
-     this.adddt = event;
-}
+  close() {
+    this.displayDialog = false;
+    this.dtdisplayDialog = false;
+    this.warningDisplayDialog = false;
+    this.dtchdisplayDialog = false;
+  }
 
+  dtsave() {
+    this.masterDetail.dtsave(this.adddt);
+    this.dtdisplayDialog = false;
+  }
 
+  DtRowSelect(event) {
+    this.adddt = event;
+    this.dtchdisplayDialog = true;
+  }
 
-dtmodify(){
-  this.masterDetail.dtmodify(this.adddt);
-  console.log(this.inputData)
-     this.dtchdisplayDialog=false;
-
-}
-
-
-ngOnChanges() {
-
-  //Called before any other lifecycle hook. Use it to inject dependencies, but avoid any serious work here.
-  //Add 'implements OnChanges' to the class.
-  
-}
-
-editDetail(event){
-this.inputData=event;
-/*console.log(event);
-console.log(this.inputData);*/
-}
-//結束
-
-
+  dtmodify() {
+    this.masterDetail.dtmodify(this.adddt);
+    this.dtchdisplayDialog = false;
+  }
+  ngOnChanges() {
+  }
 }
 
 
--- src/app/master-detail03/master-detail.component.ts
+++ src/app/master-detail03/master-detail.component.ts
@@ -12,206 +12,97 @@ import { Dt, Mt, MasterDetail } from "./models";
   templateUrl: './master-detail.component.html',
   styleUrls: ['./master-detail.component.scss']
 })
-export class MasterDetailComponent implements OnInit, OnChanges {
+export class MasterDetailComponent implements OnInit {
   constructor(public toastr: ToastsManager, private vRef: ViewContainerRef) {
     this.toastr.setRootViewContainerRef(vRef);
   }
   @Input() testmtDatas: Mt[] = [];
-  @Input() testdtDatas: Dt[] = [];
-  @Input() testdtDatas1: Dt[] =
-  [{ "c1": 1, "c2": 7, "c3": 3 },
-  { "c1": 2, "c2": 5, "c3": 6 },
-  { "c1": 3, "c2": 8, "c3": 9 }];
-  @Input() testdtDatas2: Dt[] = [];
-  @Input() testdtColumns: Object[] = [{ field: "c1", header: "c1" },
-  { field: "c2", header: "c2" },
-  { field: "c3", header: "c3" }];
-  ;;
-
-
+  @Input() testdtColumns: Object[] = [];
   @Input() dtIndex;
   @Input() mtIndex;
   @Input() mtSelection: string = "multiple";
-  @Input() mtIndexValue;
   @Input() contentDisplay: boolean = false;
   @Input() contentHeight: number = 30;
-  @Output() onMtRowSelect = new EventEmitter<any>()
   @Output() onDtRowSelect = new EventEmitter<any>();
-  @Output() onEditDetail = new EventEmitter();
+
 
   private masterWidth: string = "";
   private rightDivWidth: string = "";
   private contentDivHeight: string = "";
 
-  private testdtDatas3 = [];
-  value: Mt = new Mt();
-
   adddt: Dt = new Dt();
   selectedDt;
-
-
-  dtDatas;
-
-
-
-dtDataToRender:any[];
-
-
-
+  dtDataToRender: any[];
 
   //測試區
 
-  @Input() inputData: MasterDetail[] = [
-
-  ]
-
+  @Input() inputData: MasterDetail[] =  [];
   selectMt: MasterDetail[];
-  selectedMt;
-  //測試區結束
+  selectedMt:MasterDetail;
+
+  ngOnInit() {
+    this.masterWidth = `25vw`;
+    this.rightDivWidth = `70vw`;
+    this.contentDivHeight = `${this.contentHeight}vh`;
+  }
+
+  //更新子組件dt dataTable
+  private dtUpdateDataToRender(datasource) {
+    if (datasource) {
+      this.dtDataToRender = [];
+      datasource.forEach(element => { this.dtDataToRender.push(element) });
+    }
+    else {
+      this.dtDataToRender = datasource;
+    }
+  }
 
   //mt新增
   mtsave(value) {
     let newData: MasterDetail[] = this.inputData.concat({ master: value, detail: [] });
-    this.inputData = [];
     this.inputData = newData;
     this.toastr.success('新增成功!', 'Success!');
   }
-  //master刪除
+
+  //mt刪除
   mtdelete(rowValue) {
     let index: number = this.inputData.indexOf(rowValue);
     this.inputData = this.inputData.filter((val, i) => i != index);
     this.toastr.warning('刪除成功', 'Success!');
   }
-  ngOnInit() {
-    this.masterWidth = `25vw`;
-    this.rightDivWidth = `70vw`;
-    this.contentDivHeight = `${this.contentHeight}vh`;
-  
-  }
-  //mt找到項位
-  findSelectedMtIndex(): number {
-    return this.inputData.indexOf(this.selectedMt);
-  }
-  //dt找到項位
-  findSelectedDtIndex(): number {
-    return this.selectedMt.detail.indexOf(this.selectedDt[0]);
-  }
-  //dt新增
 
+  //dt新增
   dtsave(adddt) {
     let newDetail = this.selectedMt['detail'].concat(adddt);
-    this.selectedMt['detail'] = [];
     this.selectedMt['detail'] = newDetail;
-     this.dtUpdateDataToRender(this.selectedMt.detail);
+    this.dtUpdateDataToRender(this.selectedMt.detail);
     this.toastr.success('新增成功!', 'Success!');
   }
-
   //dt修改
   dtmodify(adddt) {
     let mtIndex = this.findSelectedMtIndex();
     let dtIndex = this.findSelectedDtIndex();
-    //this.inputData[mtIndex].detail[dtIndex] = adddt;
-    
-    this.selectedMt.detail[dtIndex]=adddt;
+    this.selectedMt.detail[dtIndex] = adddt;
     this.dtUpdateDataToRender(this.selectedMt.detail);
     this.toastr.success('修改成功!', 'Success!');
-
   }
-
-
-  ngOnChanges() {
-
-  }
-
-
-
-  private dtUpdateDataToRender(datasource) {
-
-        if(datasource) {
-            this.dtDataToRender = [];
-            for(let i = 0; i < datasource.length; i++) {            
-                this.dtDataToRender.push(datasource[i]);
-            }
-        }
-        else {
-            this.dtDataToRender = datasource;
-        }
- }
-
-
-
-
-
-
-  /*
-    public dtremain() {
-      if (this.mtIndexValue == this.testmtDatas[0].value) {
-        this.testdtDatas1 = this.testdtDatas;
-      }
-      else if (this.mtIndexValue == this.testmtDatas[1].value) {
-        this.testdtDatas2 = this.testdtDatas;
-      }
-      else {
-        this.testdtDatas = [];
-      }
-    };
-  */
-
-  /*
-  空陣列方法
-  let temp=this.inputData;
-  this.inputData=[];
-  temp[x].detail[y]=adddt;
-  this.inputData=temp;
-  
-  */
-
-
-  /*private inputData:MasterDetail[] =[
-  {     master: 1 ,detail:[{ c1: 1, c2: 7, c3: 3 },
-    { c1: 2, c2: 5, c3: 6 },
-    { c1: 3, c2: 8,c3: 9 }] },
-    {master:2,detail:[{ c1: 1, c2: 22, c3: 33 },
-    { c1: 21, c2: 55, c3: 66 },
-    { c1: 34, c2: 88, c3: 99 }]}
-  
-  ] 
-  
-  */
-
-
-
-
-
-
 
   //dt刪除
   dtdelete(rowValue) {
     this.selectedMt['detail'] = this.selectedMt['detail'].filter(value => { return value != rowValue });
-     this.dtUpdateDataToRender(this.selectedMt.detail);
+    this.dtUpdateDataToRender(this.selectedMt.detail);
     this.toastr.warning('刪除成功', 'Success!');
   }
-
-
-
-
-
-
-
-
-
 
   //主檔選單一筆
   public mtRowSelected(event) {
     this.selectedMt = this.selectMt[0];
+    console.log(this.selectedMt)
     this.dtUpdateDataToRender(this.selectedMt.detail);
-    
   }
-
 
   //明細連點選單一筆
   DtRowSelect(event) {
-
     this.adddt = this.cloneDt(event.data);
     this.onDtRowSelect.emit(this.adddt);
   }
@@ -224,6 +115,18 @@ dtDataToRender:any[];
     }
     return dt;
   }
+  
+  //mt找到項位
+  findSelectedMtIndex(): number {
+    return this.inputData.indexOf(this.selectedMt);
+  }
+
+  //dt找到項位
+  findSelectedDtIndex(): number {
+    return this.selectedMt.detail.indexOf(this.selectedDt[0]);
+  }
+
+
 
 }
 
